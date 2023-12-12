2ac8150863a0adcd641f3841e49f16b6068ba81e
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-12-11 23:31:10 -0800

    Day 12 part 2

--- kotlin/src/main/kotlin/day12/Day12.kt
+++ kotlin/src/main/kotlin/day12/Day12.kt
@@ -2,6 +2,8 @@ package day12
 
 import shared.getInputFile
 
+data class ArrangementSearchInput(val hashes: String, val numHashes: List<Int>, val hashesStart: Int)
+
 fun main() {
     val input = getInputFile(12).readText().trim()
 
@@ -10,17 +12,41 @@ fun main() {
         println()
         println(line)
         val numHashes = numHashesStr.split(",").map { it.toInt() }
-        val numArrangements = numPossibleArrangements(hashes, numHashes)
+        val numArrangements = numPossibleArrangements(hashes, numHashes, cache = mutableMapOf())
         println("Arrangements = $numArrangements")
         numArrangements
     }.let { println("Part 1: $it") }
+
+    // Part 2
+    input.lines().sumOf { line ->
+        val (hashes, numHashesStr) = line.split(" ")
+        val hashesRepeated = (0..<5).joinToString("?") { hashes }
+        println()
+        println(line)
+        val numHashes = numHashesStr.split(",").map { it.toInt() }
+        // Also repeat the number of hashes
+        val numHashesRepeated = (0..<5).flatMap { numHashes }
+        println("$hashesRepeated $numHashesRepeated")
+
+        val numArrangements = numPossibleArrangements(hashesRepeated, numHashesRepeated, cache = mutableMapOf())
+        println("Arrangements = $numArrangements")
+        numArrangements
+    }.let { println("Part 2: $it") }
+    // 4093521072395 too low??? oh no
+    // 28606137449920 :) better
 }
 
 /**
  * We have a string like ".??..??...?##." and a number of hashes in a row,
  * and we need to determine how many ways can we arrange the hashes.
  */
-fun numPossibleArrangements(hashes: String, numHashes: List<Int>, hashesStart: Int = 0): Long {
+fun numPossibleArrangements(
+    hashes: String,
+    numHashes: List<Int>,
+    hashesStart: Int = 0,
+    cache: MutableMap<ArrangementSearchInput, Long>
+): Long {
+    // Probably faster to handle the base cases without the cache.
     // Base cases: no hashes left to place
     if (numHashes.isEmpty()) {
         // Are there any hashes still left tho?
@@ -29,6 +55,10 @@ fun numPossibleArrangements(hashes: String, numHashes: List<Int>, hashesStart: I
 
     // No string left but more hashes to place
     if (hashesStart >= hashes.length) return 0L
+
+    // Check the cache
+    val input = ArrangementSearchInput(hashes, numHashes, hashesStart)
+    if (input in cache) return cache[input]!!
 
     // Try placing the first set of hashes at each possible position.
     val currentGroup = numHashes.first()
@@ -40,11 +70,14 @@ fun numPossibleArrangements(hashes: String, numHashes: List<Int>, hashesStart: I
 
         val sub = hashes.substring(i, i + currentGroup)
         val next = hashes.elementAtOrElse(i + currentGroup) { '.' }
-        if (sub.all { it == '#' || it == '?'} && next != '#') {
+        if (sub.all { it == '#' || it == '?' } && next != '#') {
             // Try placing the remaining hashes
-            numWays += numPossibleArrangements(hashes, remainingGroups, i + currentGroup + 1)
+            numWays += numPossibleArrangements(hashes, remainingGroups, i + currentGroup + 1, cache)
         }
     }
+
+    // Cache the result
+    cache[input] = numWays
 
     return numWays
 }
