04d0a872e7453c23a7276a79b67d64c0c3d17ed1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-12-24 07:28:08 +1100

    Day 19 part 2

--- kotlin/src/main/kotlin/day19/Day19.kt
+++ kotlin/src/main/kotlin/day19/Day19.kt
@@ -15,12 +15,43 @@ val rulePattern = Regex("""(\w+)\{(.+)\}""")
 
 val partSectionPattern = Regex("""(\w)=(\d+)""")
 
+typealias RuleRange = Map<String, IntRange>
+
+data class RuleRangeAndDest(val ruleRange: RuleRange, val dest: String)
+
+data class SplitResult(val mapped: RuleRangeAndDest, val leftOver: RuleRange)
+
 data class Condition(val property: String, val inequality: Inequality, val compareAgainst: Int, val dest: String) {
 
     fun matches(part: Map<String, Int>): Boolean = when (inequality) {
         Inequality.LESS_THAN -> part[property]!! < compareAgainst
         Inequality.GREATER_THAN -> part[property]!! > compareAgainst
         Inequality.ANYTHING -> true
+    }
+
+    fun splitRuleRange(ruleRange: RuleRange): SplitResult {
+        if (inequality == Inequality.ANYTHING) {
+            return SplitResult(RuleRangeAndDest(ruleRange, dest), emptyMap())
+        }
+        val range = ruleRange[property]!!
+        val (newRange, otherRange) = when (inequality) {
+            Inequality.LESS_THAN -> {
+                val newRange = range.first..(compareAgainst - 1)
+                val otherRange = compareAgainst..range.last
+                newRange to otherRange
+            }
+            Inequality.GREATER_THAN -> {
+                val newRange = (compareAgainst + 1)..range.last
+                val otherRange = range.first..compareAgainst
+                newRange to otherRange
+            }
+            else -> throw IllegalStateException("Shouldn't be here")
+        }
+        val newRuleRange = ruleRange.toMutableMap()
+        newRuleRange[property] = newRange
+        val otherRuleRange = ruleRange.toMutableMap()
+        otherRuleRange[property] = otherRange
+        return SplitResult(RuleRangeAndDest(newRuleRange, dest), otherRuleRange)
     }
 
     companion object {
@@ -43,6 +74,20 @@ data class Condition(val property: String, val inequality: Inequality, val compa
 data class Rule(val name: String, val conditions: List<Condition>) {
     fun getNextRule(part: Map<String, Int>): String {
         return conditions.first { it.matches(part) }.dest
+    }
+
+    fun applyToRuleRange(ruleRange: RuleRange): List<RuleRangeAndDest> {
+        val newRanges = mutableListOf<RuleRangeAndDest>()
+        var leftOverRange = ruleRange
+        for (condition in conditions) {
+            val (newRange, otherRange) = condition.splitRuleRange(leftOverRange)
+            newRanges.add(newRange)
+            if (otherRange.isEmpty()) {
+                break
+            }
+            leftOverRange = otherRange
+        }
+        return newRanges
     }
 }
 
@@ -74,4 +119,37 @@ fun main() {
     }.sumOf { part -> part.values.sum() }
 
     println("Part 1: $part1")
+
+    // Part 2:
+    val fullRange = mapOf(
+        "x" to 1..4000,
+        "m" to 1..4000,
+        "a" to 1..4000,
+        "s" to 1..4000,
+    )
+    val toBeSplit = mutableListOf(RuleRangeAndDest(fullRange, "in"))
+    val accepted = mutableListOf<RuleRange>()
+    while (toBeSplit.isNotEmpty()) {
+        println("To be split: $toBeSplit")
+        println("Accepted: $accepted")
+        val (ruleRange, ruleName) = toBeSplit.removeAt(0)
+        val rule = rules[ruleName]!!
+        val newRanges = rule.applyToRuleRange(ruleRange)
+        for (newRange in newRanges) {
+            if (newRange.ruleRange.isEmpty()) {
+                continue
+            }
+            when (newRange.dest) {
+                "A" -> accepted.add(newRange.ruleRange)
+                "R" -> {} // Drop rejected.
+                else -> toBeSplit.add(newRange)
+            }
+        }
+    }
+
+    val part2 = accepted.sumOf {
+        // Multiply the number of elements in each range.
+        it.values.fold(1L) { acc, range -> acc * range.count() }
+    }
+    println("Part 2: $part2")
 }
