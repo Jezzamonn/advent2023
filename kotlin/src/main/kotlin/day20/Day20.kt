28d9c46509be3deadfac7e05147a9b82935b0ed1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-01-12 10:23:05 +1100

    Day 20 part 2

--- kotlin/src/main/kotlin/day20/Day20.kt
+++ kotlin/src/main/kotlin/day20/Day20.kt
@@ -1,6 +1,7 @@
 package day20
 
 import shared.getInputFile
+import java.io.File
 
 enum class Pulse {
     LOW, HIGH
@@ -30,7 +31,7 @@ class NullModule(name: String, outputs: List<String>) : Module(name, outputs) {
 
 class FlipFlopModule(name: String, outputs: List<String>) : Module(name, outputs) {
 
-    private var isOn = false
+    var isOn = false
 
     override fun handlePulse(source: String, pulse: Pulse): List<PulseDestination> {
         if (pulse == Pulse.LOW) {
@@ -67,18 +68,32 @@ class BroadcastModule(name: String, outputs: List<String>) : Module(name, output
 
 val modulePattern = Regex("""(.+) -> (.+)""")
 
+data class Chain(val conjunctionModule: ConjunctionModule, val flipFlopModules: List<FlipFlopModule>)
+
 fun main() {
     val file = getInputFile(20)
     val input = file.readText().trim()
 
+    // writeDotFile(file)
+
+//    solvePart1(input)
+
+    solvePart2(input)
+}
+
+fun writeDotFile(file: File) {
+    val input = file.readText().trim()
     // Helper: Write a dot file to visualize the modules
     val dotContents = inputToDotFile(input)
     println(dotContents)
-//    // If the input file is demo1.txt, write to demo1.dot
-//    val dotFile = file.nameWithoutExtension + ".dot"
-//    file.resolveSibling(dotFile).writeText(dotContents)
+    // If the input file is demo1.txt, write to demo1.dot
+    val dotFileName = file.nameWithoutExtension + ".dot"
+    val dotFile = file.resolveSibling(dotFileName)
+    println("Writing to $dotFile")
+    dotFile.writeText(dotContents)
+}
 
-    // Part 1:
+fun solvePart1(input: String) {
     val modules = buildModules(input)
     // Simulate 1000 button presses
     val numPulses = mutableMapOf(Pulse.LOW to 0L, Pulse.HIGH to 0L)
@@ -95,28 +110,115 @@ fun main() {
     }
     val part1 = numPulses[Pulse.LOW]!! * numPulses[Pulse.HIGH]!!
     println("Part 1: $part1")
+}
 
-    // Part 2:
-//    val modules2 = buildModules(input)
-//    for (i in generateSequence(1) { it + 1 }) {
-//        val sentToRx = mutableMapOf(Pulse.LOW to 0L, Pulse.HIGH to 0L)
-//        val toSend = ArrayDeque(listOf(PulseDestination("button", "broadcaster", Pulse.LOW)))
-//        while (toSend.isNotEmpty()) {
-//            val next = toSend.removeFirst()
-//            if (next.dest == "rx" && next.pulse == Pulse.LOW) {
-//                sentToRx[next.pulse] = sentToRx[next.pulse]!! + 1
-//            }
-//
-//            val module = modules2[next.dest]!!
-//            val newPulses = module.handlePulse(next.source, next.pulse)
-//            toSend.addAll(newPulses)
-//        }
-//
-//        if (sentToRx == mapOf(Pulse.LOW to 1L, Pulse.HIGH to 0L)) {
-//            println("Part 2: $i")
-//            break
-//        }
-//    }
+fun solvePart2(input: String) {
+    val modules = buildModules(input)
+
+    // To start with, lets draw a coloured graph of the modules.
+    // To help with the layout, we find the chains of % modules first.
+    val chainStarts = modules["broadcaster"]!!.outputs.map { modules[it]!! as FlipFlopModule }
+    val chains = chainStarts.map { start ->
+        val flipFlopModules = mutableListOf<FlipFlopModule>()
+        var current: FlipFlopModule? = start
+        while (current != null) {
+            flipFlopModules.add(current)
+            current = current.outputs.map { modules[it]!! }.firstOrNull { it is FlipFlopModule } as? FlipFlopModule
+        }
+        val conjunctionModule = modules.values.first { it.outputs.contains(start.name) && it is ConjunctionModule } as ConjunctionModule
+        Chain(conjunctionModule, flipFlopModules)
+    }
+
+    // Clear the console
+    print("\u001B[2J")
+
+    // Simulate some pulses and animate each chain.
+    // Just do an infinite loop for now.
+    while (true) {
+        val toSend = ArrayDeque(listOf(PulseDestination("button", "broadcaster", Pulse.LOW)))
+        var hadConjunctionPulse = false
+        while (toSend.isNotEmpty()) {
+            val next = toSend.removeFirst()
+            if (setOf("hd", "fl", "kc", "tb").contains(next.source) && next.pulse == Pulse.LOW) {
+                hadConjunctionPulse = true
+            }
+            val module = modules[next.dest]!!
+            val newPulses = module.handlePulse(next.source, next.pulse)
+            toSend.addAll(newPulses)
+
+//            Thread.sleep(10)
+        }
+        if (hadConjunctionPulse) {
+            showChainsAnimationFrame(chains, modules)
+            Thread.sleep(1000)
+        }
+    }
+}
+
+/** Move the cursor to the top of the console and draws the chains. */
+fun showChainsAnimationFrame(chains: List<Chain>, modules: Map<String, Module>) {
+    // Move to the top (don't clear)
+    print("\u001B[0;0H")
+    // Draw the chains
+    println(drawChains(chains, modules))
+}
+
+/** Draws things to the console output. */
+fun drawChains(chains: List<Chain>, modules: Map<String, Module>): String {
+    // Chains will look like this:
+    //
+    //     |             |
+    //     v             v
+    // -> %nd -> %nd -> %nd -> %nd
+    //            |             |
+    //            v             v
+    //
+    //     |             |
+    //     v             v
+    // -> %nd -> %nd -> %nd -> %nd
+    //            |             |
+    //            v             v
+    //
+    // Where nodes have arrows coming from the top if they have an input from the & node,
+    // and arrows on the bottom if they have an output from the & node.
+
+    // Ansi colors codes
+    val grey = "\u001B[90m"
+    val normal = "\u001B[0m"
+
+    return chains.joinToString("\n\n") { chain ->
+        // Everything is grey by default.
+        val inputsLine1 = StringBuilder(grey)
+        val inputsLine2 = StringBuilder(grey)
+        val nodesLine = StringBuilder(grey)
+        val outputsLine1 = StringBuilder(grey)
+        val outputsLine2 = StringBuilder(grey)
+        for (node in chain.flipFlopModules) {
+            val hasInputFromConjunctionModule = chain.conjunctionModule.outputs.contains(node.name)
+            val hasOutputToConjunctionModule = node.outputs.contains(chain.conjunctionModule.name)
+
+            if (hasInputFromConjunctionModule) {
+                inputsLine1.append("     | ")
+                inputsLine2.append("     v ")
+            } else {
+                inputsLine1.append("       ")
+                inputsLine2.append("       ")
+            }
+            // If the module is off, draw it in grey using the ansi escape code for grey.
+            val color = if (node.isOn) normal else grey
+            nodesLine.append(" -> $color%${node.name}$grey")
+
+            if (hasOutputToConjunctionModule) {
+                outputsLine1.append("     | ")
+                outputsLine2.append("     v ")
+            } else {
+                outputsLine1.append("       ")
+                outputsLine2.append("       ")
+            }
+        }
+        // Return all the lines as a single string
+        listOf(inputsLine1, inputsLine2, nodesLine, outputsLine1, outputsLine2).joinToString("\n")
+    }
 }
 
 fun inputToDotFile(input: String): String {
--- kotlin/src/main/kotlin/shared/Inputs.kt
+++ kotlin/src/main/kotlin/shared/Inputs.kt
@@ -7,7 +7,9 @@ fun getInputFile(day: Int): File {
     // Then search recursive for a folder named "dayX" in the current directory,
     // and then the filename in that folder.
     val filename = readln().ifEmpty { "input" }
-    val folder = File(".").walk().find { it.name == "day$day" } ?: throw Exception("Could not find folder day$day")
+    val folder = File(".").walk()
+        // Exclude the out folder
+        .find { it.name == "day$day" && !it.path.contains("out")} ?: throw Exception("Could not find folder day$day")
     return folder.listFiles()?.find { it.name.contains(filename) && it.name.endsWith(".txt") }
         ?: throw Exception("Could not find file $filename in folder day$day")
 }
